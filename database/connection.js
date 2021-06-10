1d6cfb3ff5e05bb950d025f37b903e70dd955ba3
Ginato5 <68754503+Ginato5@users.noreply.github.com>
2021-06-10 21:28:36 +0200

    Connected to azure database

--- database/connection.js
+++ database/connection.js
@@ -1,22 +1,22 @@
 const mysql = require('mysql2/promise');
 
-const config = {
-    host: 'localhost',
-    user: 'root',
-    password: 'Azerty123',
-    database: 'betterdb',
-};
-
 // const config = {
-//     host: "betterdb.mysql.database.azure.com",
-//     user: "david@betterdb",
+//     host: 'localhost',
+//     user: 'root',
 //     password: 'Azerty123',
 //     database: 'betterdb',
-//     port: 3306,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
 // };
+
+const config = {
+    host: "betterdb.mysql.database.azure.com",
+    user: "david@betterdb",
+    password: 'Azerty123',
+    database: 'betterdb',
+    port: 3306,
+    ssl: {
+        rejectUnauthorized: false
+    }
+};
 
 async function getDatabaseConnection() {
     return mysql.createConnection(config);
